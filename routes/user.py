a0fce993366fe110afbb55c7b4e3d77743660371
Giiid <gideonfummey1@gmail.com>
2024-04-26 00:29:08 +0100

    user registration

--- routes/user.py
+++ routes/user.py
@@ -1,6 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from config.config import get_db
+from config.schema import UserInModel, UserOutModel
+from models.models import User
 
 
 user = APIRouter()
 
 
+@user.post("/user/register", response_model=UserOutModel)
+async def create_user(user: UserInModel, db: Session = Depends(get_db) ):
+    new_user = User(username=user.username, email=user.email, password=user.password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    
