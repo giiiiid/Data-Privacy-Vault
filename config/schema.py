7d18ce8a813a87de508bdf82e1d52bd5da353ab4
Giiid <gideonfummey1@gmail.com>
2024-04-26 00:19:30 +0100

    schema

---
+++ config/schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+
+
+class UserModel(BaseModel):
+    username: str
+    email: str
+
+
+class UserInModel(UserModel):
+    password: str
+
+
+class UserOutModel(UserModel):
+    pass
