74c349af844c56b0c387002a4d3653a3c85e9eb1
Giiid <gideonfummey1@gmail.com>
2024-04-28 10:29:54 +0100

    debugging the decryption

--- hello.py
+++ hello.py
@@ -14,16 +14,26 @@ message = "Hello world"
 
 cipher = AES.new(key, AES.MODE_CBC)
 
-ciphered_message = cipher.encrypt(pad(message.encode("utf-8"), AES.block_size))
-# print(ciphered_message)
+ciphered_message = cipher.encrypt(pad(message.encode("utf-8")[::16], AES.block_size))
+print(ciphered_message)
 
 
 ''''Decrypting'''
-iv = b'\xb7\x84\xbc\x96\x00({\x99\x10\x02\xd6_\x12Z\x10\x8a'
-decrpyt_data = b'\xb7\x84\xbc\x96\x00({\x99\x10\x02\xd6_\x12Z\x10\x8a'
+# iv = b'\xb7\x84\xbc\x96\x00({\x99\x10\x02\xd6_\x12Z\x10\x8a' // incorrect iv
+decrpyted_data = b'\xb7\x84\xbc\x96\x00({\x99\x10\x02\xd6_\x12Z\x10\x8a'
 
 cipher = AES.new(key, AES.MODE_CBC, iv=cipher.iv)
 
-original = unpad(cipher.decrypt(decrpyt_data), AES.block_size)
-print(original)
-# print(iv.encode("utf-8"))
+original = cipher.decrypt(unpad(decrpyted_data, AES.block_size))
+print(original.decode("utf-8"))
+
+
+def encrypt(plain_text, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    b = plain_text.encode("UTF-8")
+    return cipher.iv, cipher.encrypt(pad(b, AES.block_size))
+
+# if __name__ == "__main__":
+#     iv, enc_text = encrypt(message[::16], key)
+#     print(iv)
+#     print(enc_text)
